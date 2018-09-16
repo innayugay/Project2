b34d3c7323bf52e7fa06c9c54414f00b9646cbcb
InnaYugay <yuinnaa@gmail.com>
2018-09-16 03:42:47 -0400

    user model working, authentication is functioning

---
+++ routes/users/authRoutes.js
@@ -0,0 +1,81 @@
+const express      = require('express');
+const router       = express.Router();
+const bcrypt       = require("bcryptjs");
+const bcryptSalt   = 10;
+const User         = require('../../models/User');
+const passport     = require('passport');
+const ensureLogin  = require("connect-ensure-login");
+
+/* GET home page */
+
+
+
+router.get("/signup", (req, res, next) => {
+  res.render("userViews/signup");
+});
+
+
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    req.flash('error', 'please specify a username and password to sign up')
+    res.render("userViews/signup", { message: req.flash("error") });
+    return;
+  }
+
+  User.findOne({ username })
+  .then(user => {
+    if (user !== null) {
+      res.render("userViews/signup", { message: req.flash("error") });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    User.create({
+        username: username,
+        password: hashPass
+    })
+    .then((response)=>{
+      res.redirect("/");
+    })
+    .catch((err)=>{
+      res.render("userViews/signup", { message: req.flash("error") });
+    })
+  })
+  .catch(error => {
+    next(error)
+  })
+});
+
+
+router.get('/login', (req, res, next)=>{
+    res.render('userViews/login', {message: req.flash('error')})
+})
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: "/private",
+  failureRedirect: "/login",
+  failureFlash: true,
+  successFlash: true,
+  passReqToCallback: true
+}));
+
+router.get('/private' , ensureLogin.ensureLoggedIn('/signup'),(req, res, next)=>{
+  console.log(req.user.username);
+  res.render('userViews/private', {message: req.flash('success'), theName: req.user.username})
+})
+
+router.get('/logout', (req, res, next)=>{
+    req.logout()
+  res.redirect('/')
+})
+
+
+
+
+module.exports = router;
